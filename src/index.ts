74824ba75da4f068ee2c6e0e70e5985c0dfedafc
Jesse Sanders <jesse.sanders@briebug.com>
2017-01-27 05:20:14 -0700

    feat(build): add npmrc for npm publish to have credentials

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,32 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
-import { DataTable} from './components/table.component';
-import { DataTableColumn } from './components/column.component';
-import { DataTableRow } from './components/row.component';
-import { DataTablePagination } from './components/pagination.component';
-import { DataTableHeader } from './components/header.component';
+import {DataTable} from './components/table.component';
+import {DataTableColumn} from './components/column.component';
+import {DataTableRow} from './components/row.component';
+import {DataTablePagination} from './components/pagination.component';
+import {DataTableHeader} from './components/header.component';
 
-import { PixelConverter } from './utils/px';
-import { Hide } from './utils/hide';
-import { MinPipe } from './utils/min';
+import {PixelConverter} from './utils/px';
+import {Hide} from './utils/hide';
+import {MinPipe} from './utils/min';
 
 export * from './components/types';
 export * from './tools/data-table-resource';
 
-export { DataTable, DataTableColumn, DataTableRow, DataTablePagination, DataTableHeader };
-export const DATA_TABLE_DIRECTIVES = [ DataTable, DataTableColumn ];
+export {DataTable, DataTableColumn, DataTableRow, DataTablePagination, DataTableHeader};
+export const DATA_TABLE_DIRECTIVES = [DataTable, DataTableColumn];
 
 
 @NgModule({
-    imports: [ CommonModule, FormsModule ],
+    imports: [CommonModule, FormsModule],
     declarations: [
         DataTable, DataTableColumn,
         DataTableRow, DataTablePagination, DataTableHeader,
         PixelConverter, Hide, MinPipe
     ],
-    exports: [ DataTable, DataTableColumn ]
+    exports: [DataTable, DataTableColumn]
 })
-export class DataTableModule { }
+export class DataTableModule {
+}
